55d91bfebfb9ed1a359f1839f5d9f39c36ee24e9
Zaphodious <achythlook@gmail.com>
2018-11-09 17:02:55 -0900

    Add bit better query

--- wurmstore.py
+++ wurmstore.py
@@ -1,4 +1,5 @@
 import sqlite3
+from contextlib import suppress
 
 class WurmStore:
 
@@ -7,6 +8,13 @@ class WurmStore:
     
     def _cursor(self):
         return self._connection.cursor()
+    
+    def _maketables(self):
+        with suppress(Exception):
+            c = self._connection
+            c.execute("CREATE TABLE IF NOT EXISTS facts ('factID' TEXT PRIMARY KEY, 'factName' TEXT, 'factValue' TEXT)")
+            c.execute("CREATE TABLE IF NOT EXISTS transactions ('transactionID' TEXT PRIMARY KEY, 'factID' TEXT, 'transactionOperation' TEXT, 'transactionDate' TEXT)")
+
 
     def foo(self):
         pass
